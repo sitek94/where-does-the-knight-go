7f4f1b00c699f529766489f33c80230ebd4ac561
Maciek <msitkowski94@gmail.com>
2022-06-14 22:14:48 +0200

    update svelte config to use static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static'
+import preprocess from 'svelte-preprocess'
+
+const isDev = process.env.NODE_ENV === 'development'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
-};
+  kit: {
+    adapter: adapter({
+      fallback: 'index.html',
+    }),
+    paths: {
+      base: isDev ? '' : `/where-does-the-knight-go`,
+    },
+    prerender: {
+      default: false,
+    },
+  },
+}
 
-export default config;
+export default config
